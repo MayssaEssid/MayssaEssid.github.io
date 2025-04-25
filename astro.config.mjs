e3b662fee8b2c9f7f0a993506338c78356c7c90a
Mayssa Essid <essidmayssa4997@gmail.com>
2025-04-25 16:01:35 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-    site: 'https://MayssaEssid.github.io',
-    outDir: '../dist'  // Chemin de sortie explicite
-  })
+  site: 'https://MayssaEssid.github.io',
+  // base: '/my-repo/',  // À supprimer ou commenter pour GitHub Pages personnalisé
+  outDir: 'dist',
+  build: {
+    format: 'directory'
+  }
+})
